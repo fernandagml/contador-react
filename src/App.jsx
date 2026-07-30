0195b4a4142522a91c76d0d90a15b5ba1cb5b20f
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-07-30 15:46:16 -0300

    Finalização da estrutura básica do componente Tema.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css';
 export default function App() {
   return (
     <>
+    <Tema />
     </>
   );
 };
--- src/components/Tema/Tema.jsx
+++ src/components/Tema/Tema.jsx
@@ -1,9 +1,30 @@
 import { useEffect, useState } from "react";
 import styles from "./Tema.module.css";
 
-export default function(){
+export default function Tema(){
+    const [contador, setContador] = useState(0);
+
+    const [nome, setNome] = useState("");
+
+    const [temaEscuro, setTemaEscuro] = useState(false);
+
+    useEffect(() => {}, []);
+
+    useEffect(() => {}, [contador]);
+
+    useEffect(() => {}, [temaEscuro]);
+
+    function aumentarContador() {
+
+    }
+
+    function diminuirContador(){}
+
+    function alterarTema(){}
+
     return(
         <>
+        <main></main>
         </>
     );
 }
