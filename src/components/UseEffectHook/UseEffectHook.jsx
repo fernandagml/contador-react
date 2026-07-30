465c2803dc376816177b4db72d56b69036d91afe
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-07-30 14:43:53 -0300

    Desenvolvimento de alguns useEffect para entender o funcionamento.

--- src/components/UseEffectHook/UseEffectHook.jsx
+++ src/components/UseEffectHook/UseEffectHook.jsx
@@ -2,9 +2,18 @@ import { useEffect, useState } from "react";
 import styles from "./UseEffectHook.module.css";
 
 export default function UseEffectHook(){
+    const [contador, setContador] = useState(0);
+    const titulo = 'Clicou ';
+
+    console.log('Sempre ocorre, mas antes do useEffect.');
+
+    useEffect(() => {console.log('Ocorre quando renderizar.');});
+
+    useEffect(() => {console.log('Ocorre quando renderizar apenas na primeira vez.');}, []);
+
+    useEffect(() => {console.log('Ocorre quando renderizar o Contador.');}, [contador]);
+
     return(
-        <>
-        <h1>Oi</h1>
-        </>
+        <button className={styles.aut} onClick={() => setContador(contador + 1)}>{contador}</button>
     );
 }
