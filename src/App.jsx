f05f7ba7a55683be3f193aa5cc04d9ff4d580a33
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-07-30 14:01:58 -0300

    Criação de novo componente para testar o UseEffectHook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Contador from './components/Contador/Contador';
+import UseEffectHook from './components/UseEffectHook/UseEffectHook';
 import './App.css';
 
 export default function App() {
   return (
     <>
       <Contador />
+      <UseEffectHook/>
     </>
   );
 };
---
+++ src/components/UseEffectHook/UseEffectHook.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import styles from "./UseEffectHook.module.css";
+
+export default function UseEffectHook(){
+    return(
+        <>
+        <h1>Oi</h1>
+        </>
+    );
+}
