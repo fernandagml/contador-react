5349ee8d37055aa2f83424990fb35e058a0b20f4
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-07-30 14:59:54 -0300

    Criação do componente Tema.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Contador from './components/Contador/Contador';
-import UseEffectHook from './components/UseEffectHook/UseEffectHook';
+// import Contador from './components/Contador/Contador';
+// import UseEffectHook from './components/UseEffectHook/UseEffectHook';
+import Tema from './components/Tema/Tema';
 import './App.css';
 
 export default function App() {
   return (
     <>
-      <UseEffectHook/>
     </>
   );
 };
---
+++ src/components/Tema/Tema.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import styles from "./Tema.module.css";
+
+export default function(){
+    return(
+        <>
+        </>
+    );
+}
